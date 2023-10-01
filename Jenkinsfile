dc0652f8ed277f7174244783a3126056cd73b7ac
James Ohia <jamesohia4@gmail.com>
2023-10-01 16:57:35 +0100

    Adding parameters and tools

--- Jenkinsfile
+++ Jenkinsfile
@@ -4,11 +4,15 @@ pipeline{
     VERSION = "1.1"
     CREDENTIALS = credentials('Test-Script')
   }
+  parameters{
+    choice(name: 'version', choices '[1.1, 1.2, 1.3, 1.4]', description:'Version to select')
+    booleanParams(name: 'executeTest', defaultValue: true, description:'Either Test should be executed or skipped')
+  }
   stages{
     stage("Build"){
       when{
         expression{
-          BRANCH_NAME == "Dev"
+          param.executeTest
         }
       }
       steps{
@@ -25,6 +29,7 @@ pipeline{
     stage("Deploy"){
       steps{
         echo "This is the Deployment stage"
+        echo "The version we are deploying is ${param.version}"
       }
     }
   }
--- innerCredentials.groovy
+++ innerCredentials.groovy
@@ -1,13 +1,24 @@
 pipeline{
   agent any
+  tools{
+    maven 'MaveName'
+    gradle
+    jdk
+  }
   environment{
     VERSION = "1.1"
+  }
+  parameters{
+    //DataType(name, defaultValue, description)
+    string(name: 'versionMain', defaultValue: '1.1', description: 'The default Version to deploy');
+    choice(name: 'version', choices '[1.1, 1.2, 1.3, 1.4]', description:'')
+    booleanParams(name: 'executeTest', defaultValue: true, description:'Either Test should be executed or skipped')
   }
   stages{
     stage("Build"){
       when{
         expression{
-          BRANCH_NAME == "Dev"
+          BRANCH_NAME == "Dev" && param.executeTest
         }
       }
       steps{
@@ -16,6 +27,7 @@ pipeline{
         }
         echo "This is the build stage"
         echo "The credentials is ${CREDENTIALS}"
+        sh "mvn install"
       }
     }
     stage("Test"){
@@ -27,6 +39,7 @@ pipeline{
     stage("Deploy"){
       steps{
         echo "This is the Deployment stage"
+        echo "The version we are deploying to is ${param.version}"
       }
     }
   }
